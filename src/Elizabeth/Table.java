45a4fe3b486c4a06f524f58000533f216b96aee7
Omar Waleed <omarpersonalprofile@gmail.com>
2015-03-26 16:32:16 +0200

    updating the Table file with improved structure and new methods

--- src/Elizabeth/Table.java
+++ src/Elizabeth/Table.java
@@ -1,5 +1,4 @@
 package Elizabeth;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -9,8 +8,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 
-@SuppressWarnings("serial")
-class Database implements Serializable {
+class Database{
+	
+	// the sole purpose of this class is to keep track of all the table instances created
 	
 	String name;
 	ArrayList<Table> tableList;
@@ -33,39 +33,24 @@ class Database implements Serializable {
 }
 
 
-@SuppressWarnings("serial")
-public class Table implements Serializable {
-
-	public String getTableName() {
-		return tableName;
-	}
-
-	public void setTableName(String tableName) {
-		this.tableName = tableName;
-	}
-
-	public ArrayList<Page> getPageList() {
-		return pageList;
-	}
-
-	public void setPageList(ArrayList<Page> pageList) {
-		this.pageList = pageList;
-	}
+public class Table{
 
 	String tableName;
 	ArrayList <Page> pageList;
+	int numberOfPages;
 	
 	public Table(String name) {
 		tableName = name;
 		pageList = new ArrayList<Page>();
+		numberOfPages = 0;
 	}
 	
 	public void addNewPageToTable()
 	{
-		// create a new page and link it to the table. The default value is 200 entries per page
-		// this should change to read from the metadata file the number of entries per page
-		// CHANGED to be read from the metadata @Sabrout
+		// create a new page and link it to the table.
+		
 		this.getPageList().add(new Page(DBApp.getMaximumRowsCountinPage()));
+		this.setNumberOfPages(this.getNumberOfPages()+1);
 	}
 	
 //	public void readIntoNewTable(String table_Name) 
@@ -86,6 +71,7 @@ public class Table implements Serializable {
 //		
 //	}
 	
+
 	public void readTableContent()
 	{
 		//this method read all files starting with the name of the calling table
@@ -100,7 +86,8 @@ public class Table implements Serializable {
 			{
 				try
 				{
-				ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("/data/"+this.getTableName())));
+				ObjectInputStream ois = 
+						new ObjectInputStream(new FileInputStream(new File("/data/"+allFiles[i].getName())));
 		        this.getPageList().add((Page)ois.readObject());
 		        ois.close();
 				}catch(Exception e) {e.printStackTrace();}
@@ -128,28 +115,111 @@ public class Table implements Serializable {
 		
 	}
 	
-	public void writeCertainPages(ArrayList<Integer> pageNumbers)
+	public Page readOnePage(int page_number) 
 	{
-		// self explanatory really
 		
-		for (int i = 0; i < pageNumbers.size(); i++) {
-			Page p = this.getPageList().get(pageNumbers.get(i));
-			try
+		/*this method catches the exception if the page doesn't exist, but you should remove the try and catch
+		when you handle it in the main class */
+		// read a page from the calling table with certain page number in the table arraylist
+		/* most probably you will have to call this after  initializing a table and calling the method
+		   readTableContent	 */
+		
+		Page returnPage = null;
+		
+		try
+		{
+			ObjectInputStream ois = 
+					new ObjectInputStream(new FileInputStream(new File("/data/"+this.getTableName()+"_"+page_number)));
+			returnPage = (Page) ois.readObject();
+			ois.close();
+		}catch(Exception e) {e.printStackTrace();}
+		
+		
+		return returnPage;
+	}
+	
+	public void writeCertainPage(Page p, int page_number) 
+	{
+		/*this method catches the exception if the page doesn't exist, but you should remove the try and catch
+		when you handle it in the main class */
+		
+		//reads a certain page from the table and then overwrite it with the new page you called the method with
+
+		try
+		{
+
+			Page testingPage = readOnePage(page_number);
+			if(!testingPage.isDead())
 			{
-			ObjectOutputStream oos = 
-					new ObjectOutputStream(
-							new FileOutputStream(new File("/data/"+this.getTableName()+"_"+pageNumbers.get(i))));
-	        oos.writeObject(p);
-	        oos.close();
-			} catch(Exception e){e.printStackTrace();}
-			
-		}
+				ObjectOutputStream oos = 
+						new ObjectOutputStream(
+								new FileOutputStream(new File("/data/"+this.getTableName()+"_"+page_number)));
+				oos.writeObject(p);
+				oos.close();
+			}
+		} catch(Exception e){e.printStackTrace();}
+
+	}
+	
+	public void deleteEntry(int entryNumber, int pageNumber) 
+	{
+		// this reads the page number given relative to that table and deletes an entry then rewrite the page 
+		Page p = this.readOnePage(pageNumber);
+		p.getContent()[entryNumber] = null;
+		this.writeCertainPage(p, pageNumber);
+	}
+	
+	// Setters and getters
+	
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName(String tableName) {
+		
+		// we should handle table renaming here
+		
+		this.tableName = tableName;
+	}
+
+	public ArrayList<Page> getPageList() {
+		return pageList;
+	}
+
+	public void setPageList(ArrayList<Page> pageList) {
+		this.pageList = pageList;
+	}
+	
+	public int getNumberOfPages() {
+		return numberOfPages;
+	}
+
+	public void setNumberOfPages(int numberOfPages) {
+		this.numberOfPages = numberOfPages;
 	}
 
 }
 
+@SuppressWarnings("serial")
 class Page implements Serializable{
 
+	int allowedEntries;
+	Entry[] content;
+	ArrayList <Integer> emptyIndices;
+	boolean dead;
+	
+	public Page(int allowed) {
+		emptyIndices = new ArrayList<Integer>();
+		allowedEntries = allowed;
+		content = new Entry[allowed];
+		for (int i = 0; i < allowed; i++) 
+		{
+			emptyIndices.add(i);
+		}
+	}
+	
+	// Setters and getters
+	
 	public boolean isDead() {
 		return dead;
 	}
@@ -157,12 +227,7 @@ class Page implements Serializable{
 	public void setDead(boolean dead) {
 		this.dead = dead;
 	}
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
+	
 	public int getAllowedEntries() {
 		return allowedEntries;
 	}
@@ -187,27 +252,21 @@ class Page implements Serializable{
 		this.emptyIndices = emptyIndices;
 	}
 
-	int allowedEntries;
-	Entry[] content;
-	ArrayList <Integer> emptyIndices;
-	boolean dead;
 	
-	public Page(int allowed) {
-		emptyIndices = new ArrayList<Integer>();
-		allowedEntries = allowed;
-		content = new Entry[allowed];
-		for (int i = 0; i < allowed; i++) 
-		{
-			emptyIndices.add(i);
-		}
-	}
 	
 }
 
-@SuppressWarnings("serial")
-class Entry implements Serializable {
+class Entry{
 	
 	String[] values;
+	
+	public Entry(String[] v) {
+		
+		values = v;
+		
+	}
+	
+	// Setters and getters
 	
 	public String[] getValues() {
 		return values;
@@ -217,11 +276,6 @@ class Entry implements Serializable {
 		this.values = values;
 	}
 
-	public Entry(String[] v) {
-		
-		values = v;
-		
-	}
 	
 	
 	
